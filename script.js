c66e1ea1c4497ffbdc2f878313cb0a4c4d4ea79c
Dhanjit Deka <dhanjit1951@gmail.com>
2021-06-09 20:32:42 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,12 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline.to(".rock",3,{y: -500}).to(".girl",3,{y: -300}, '-=3').fromTo(".bg1",{y: -50},{y: 0,duration: 3},'-=3')
+    .to(".content",3,{top: '0%'},'-=3').fromTo(".content-images",{opacity:0},{opacity:1,duration: 3});
+
+let scene = new ScrollMagic.Scene({
+    triggerElement: "section",
+    duration: "200%" ,
+    triggerHook:0,
+}).setTween(timeline).setPin("section").addTo(controller);
+
